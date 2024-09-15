808a0582b4f2ac19707d1f520a781c4acfb2a05b
noe <noe.tehraoui1@gmail.com>
2024-09-15 14:47:48 +0200

    modif chemin img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-logos">
-        <img src="./logo-inra.png" alt="Logo 1" className="logo1" />
-        <img src="./MAPDEF.png" alt="Logo 2" className="logo2" />
-        <img src="./images.png" alt="Logo 3" className="logo3" />
-        <img src="./aryam.png" alt="Logo 4" className="logo4" />
+        <img src="/logo-inra.png" alt="Logo 1" className="logo1" />
+        <img src="/MAPDEF.png" alt="Logo 2" className="logo2" />
+        <img src="/images.png" alt="Logo 3" className="logo3" />
+        <img src="/aryam.png" alt="Logo 4" className="logo4" />
       </div>    
     </header>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Menu = () => {
   return (
     <aside className="menu">
-      <a href="./flyer.pdf" target="_blank" rel="noopener noreferrer">
+      <a href="/flyer.pdf" target="_blank" rel="noopener noreferrer">
         Programme
       </a>
       <p>Ressources Techniques</p>
