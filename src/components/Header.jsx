fb62379e037b9c6e864a3eea7e1a9ef3ff367e67
noe <noe.tehraoui1@gmail.com>
2024-09-22 22:50:37 +0200

    add new section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ const Header = () => {
     <header className="header">
       <div className="header-logos">
         <img src="./logo-inra.png" alt="Logo 1" className="logo1" />
-        <img src="./MAPDEF.png" alt="Logo 2" className="logo2" />
         <img src="./images.png" alt="Logo 3" className="logo3" />
         <img src="./aryam.png" alt="Logo 4" className="logo4" />
       </div>
--- src/pages/Ressource.jsx
+++ src/pages/Ressource.jsx
@@ -7,33 +7,49 @@ import Header from "../components/Header";
 function Ressource() {
   const resources = [
     {
-      title: "Systèmes Agroforestiers - Fiches Techniques",
-      items: [
-        { name: "Document 1", link: "/path/to/document1.pdf", thumbnail: "/path/to/thumbnail1.png" },
-        { name: "Document 2", link: "/path/to/document2.pdf", thumbnail: "/path/to/thumbnail2.png" },
-        { name: "Document 3", link: "/path/to/document3.pdf", thumbnail: "/path/to/thumbnail3.png" },
-        { name: "Document 4", link: "/path/to/document4.pdf", thumbnail: "/path/to/thumbnail4.png" },
-        { name: "Document 5", link: "/path/to/document5.pdf", thumbnail: "/path/to/thumbnail5.png" },
-        { name: "Document 6", link: "/path/to/document6.pdf", thumbnail: "/path/to/thumbnail6.png" },
-        { name: "Document 7", link: "/path/to/document7.pdf", thumbnail: "/path/to/thumbnail7.png" },
-        { name: "Document 8", link: "/path/to/document8.pdf", thumbnail: "/path/to/thumbnail8.png" },
-        { name: "Document 9", link: "/path/to/document9.pdf", thumbnail: "/path/to/thumbnail9.png" },
-        { name: "Document 10", link: "/path/to/document10.pdf", thumbnail: "/path/to/thumbnail10.png" },
-        { name: "Document 11", link: "/path/to/document11.pdf", thumbnail: "/path/to/thumbnail11.png" },
-        { name: "Document 12", link: "/path/to/thumbnail12.png", thumbnail: "/path/to/thumbnail12.png" },
+      title: "Fiches Pédagogiques",
+      subsections: [
+        {
+          title: "Fiches arbres",
+          items: [
+            { name: "Fiche Amandier", link: "./Fiche Amandier.pdf", thumbnail: "./ped.jpeg" },
+            { name: "Fiche Arganier", link: "./Fiche Arganier.pdf", thumbnail: "./ped.jpeg" },
+            { name: "Fiche Atriplex", link: "./Fiche Atriplex.pdf", thumbnail: "./ped.jpeg" },
+            { name: "Fiche Câpéprier", link: "./Fiche Câpéprier.pdf", thumbnail: "./ped.jpeg" },
+            { name: "Fiche Caroubier", link: "./Fiche Caroubier.pdf", thumbnail: "./ped.jpeg" },
+            { name: "Fiche Figuier de barbarie", link: "./Fiche Figuier de barbarie.pdf", thumbnail: "./ped.jpeg" },
+            { name: "Fiche Pistachier", link: "./Fiche Pistachier.pdf", thumbnail: "./ped.jpeg" },
+          ],
+        },
+        {
+          title: "Fiches couverts végetaux",
+          items: [
+            { name: "Fiche Legumineuses", link: "./Fiche Legumineuses.pdf", thumbnail: "./couv.jpg" },
+            { name: "Fiche Moutarde d'abyssinie", link: "./Fiche Moutarde d'abyssinie.pdf", thumbnail: "./couv.jpg" },
+            { name: "Fiche Sorgho du Soudan", link: "./Fiche Sorgho du Soudan.pdf", thumbnail: "./couv.jpg" },
+          ],
+        },
+        {
+          title: "Fiches cultures",
+          items: [
+            { name: "Fiche Orge", link: "./Fiche Orge.pdf", thumbnail: "./cult.jpg" },
+            { name: "Fiche Quinoa", link: "./Fiche Quinoa.pdf", thumbnail: "./cult.jpg" },
+            { name: "Fiche Triticale", link: "./Fiche Triticale.pdf", thumbnail: "./cult.jpg" },
+            { name: "Fiche cultures intercalaires", link: "./Fiche technique optimisation des cultures annuelles dans les oliveraies en pente.pdf", thumbnail: "./cult.jpg" },
+          ],
+        },
       ],
     },
     {
       title: "Articles de Recherches",
       items: [
-        { name: "Agroforestry in Morocco Sais case", link: "./AgroforestryinMoroccoSaiscase.pdf", thumbnail: "./background.jpg" },
-        { name: "Argan oil foot_print Bayssi et al 2024", link: "./Argan oil foot_print Bayssi et al 2024.pdf", thumbnail: "./background.jpg" },
-        { name: "Bekkaoui et al resilience AFRIMED 2024", link: "./Bekkaoui et al resilience AFRIMED 2024.pdf", thumbnail: "./background.jpg" },
-        { name: "Carbone de _sols en Afrique", link: "./Carbone_des_sols_en_Afrique.pdf", thumbnail: "./background.jpg" },
-        { name: "El Koudrim et al agroforestery AFRIMED 2024", link: "./El Koudrim et al agroforestery AFRIMED 2024.pdf", thumbnail: "./background.jpg" },
-        { name: "Gestion durable de leau et des sols au Maroc", link: "", thumbnail: "./background.jpg" },
-        { name: "Recueil des résumés Séminaire Agroforesterie", link: "./Recueil-des-résumés-Séminaire-Agroforesterie_13-juil-2023.pdf", thumbnail: "./background.jpg" },
-        { name: "Bekkaoui Congres Intern Arganier final", link: "./Bekkaoui Congres Intern Arganier final- 11.pptx", thumbnail: "./background.jpg" },
+        { name: "Agroforestry in Morocco Sais case", link: "./AgroforestryinMoroccoSaiscase.pdf", thumbnail: "./ar.jpg" },
+        { name: "Argan oil foot_print Bayssi et al 2024", link: "./Argan oil foot_print Bayssi et al 2024.pdf", thumbnail: "./ar.jpg" },
+        { name: "Bekkaoui et al resilience AFRIMED 2024", link: "./Bekkaoui et al resilience AFRIMED 2024.pdf", thumbnail: "./ar.jpg" },
+        { name: "Carbone de _sols en Afrique", link: "./Carbone_des_sols_en_Afrique.pdf", thumbnail: "./ar.jpg" },
+        { name: "El Koudrim et al agroforestery AFRIMED 2024", link: "./El Koudrim et al agroforestery AFRIMED 2024.pdf", thumbnail: "./ar.jpg" },
+        { name: "Recueil des résumés Séminaire Agroforesterie", link: "./Recueil-des-résumés-Séminaire-Agroforesterie_13-juil-2023.pdf", thumbnail: "./ar.jpg" },
+        { name: "Bekkaoui Congres Intern Arganier final", link: "./Bekkaoui Congres Intern Arganier final- 11.pptx", thumbnail: "./ar.jpg" },
       ],
     },
     {
@@ -57,16 +73,34 @@ function Ressource() {
         {resources.map((block, index) => (
           <div key={index} className="resource-block">
             <h2>{block.title}</h2>
-            <div className="thumbnails">
-              {block.items.map((item, idx) => (
-                <a key={idx} href={item.link} target="_blank" rel="noopener noreferrer">
-                  <div className="thumbnail">
-                    <img src={item.thumbnail} alt={item.name} />
-                    <p>{item.name}</p>
+            {block.subsections ? (
+              block.subsections.map((subsection, subIndex) => (
+                <div key={subIndex} className="subsection">
+                  <h3>{subsection.title}</h3>
+                  <div className="thumbnails">
+                    {subsection.items.map((item, idx) => (
+                      <a key={idx} href={item.link} target="_blank" rel="noopener noreferrer">
+                        <div className="thumbnail">
+                          <img src={item.thumbnail} alt={item.name} />
+                          <p>{item.name}</p>
+                        </div>
+                      </a>
+                    ))}
                   </div>
-                </a>
-              ))}
-            </div>
+                </div>
+              ))
+            ) : (
+              <div className="thumbnails">
+                {block.items.map((item, idx) => (
+                  <a key={idx} href={item.link} target="_blank" rel="noopener noreferrer">
+                    <div className="thumbnail">
+                      <img src={item.thumbnail} alt={item.name} />
+                      <p>{item.name}</p>
+                    </div>
+                  </a>
+                ))}
+              </div>
+            )}
           </div>
         ))}
       </div>
